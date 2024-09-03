22fce48786a24ec2a89887f11154a9189dbedc5e
Snehita Moturu <nitinmoturu72@gmail.com>
2024-09-02 20:24:13 -0700

    Q3 updated

--- payCheck.java
+++ payCheck.java
@@ -1,22 +1,27 @@
 import java.util.Scanner;
 public class payCheck {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the employee name: ");
-        String empName = in.nextLine();
-        System.out.println("Enter the Pay rate: ");
-        float empPayRate = in.nextFloat();
-        System.out.println("Enter the Hours Worked");
-        float empHrs = in.nextFloat();
-        double payCheck = 0;
-        if (empHrs > 40){
-            payCheck = (40 * empPayRate) + ((empHrs-40)*1.5*empPayRate);
-        }
+        while(true){
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter the employee name: ");
+            String empName = in.nextLine();
+            System.out.println("Enter the Pay rate: ");
+            float empPayRate = in.nextFloat();
+            if(empPayRate == 0){
+                break;
+            }
+            System.out.println("Enter the Hours Worked");
+            float empHrs = in.nextFloat();
+            double payCheck = 0;
+            if (empHrs > 40){
+                payCheck = (40 * empPayRate) + ((empHrs-40)*1.5*empPayRate);
+            }
 
-        else{
-            payCheck = (empHrs * empPayRate);
+            else{
+                payCheck = (empHrs * empPayRate);
+            }
+            
+            System.out.printf("Paycheck for " + empName + " %.2f \n", payCheck);
         }
-         
-        System.out.printf("Paycheck for " + empName + " %.2f", payCheck);
     }
 }
